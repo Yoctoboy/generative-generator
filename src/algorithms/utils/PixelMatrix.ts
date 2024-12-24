179302471ae7cc84315121241f890cba09f2c7d6
Yoctoboy <alexisr@theodo.fr>
2024-12-24 10:21:42 +0100

    type fixes

--- src/algorithms/utils/PixelMatrix.ts
+++ src/algorithms/utils/PixelMatrix.ts
@@ -6,13 +6,13 @@ export class PixelMatrix {
     height: number
     width: number
 
-    constructor(mat: [number, number, number][][]) {
+    constructor(mat: Pixel[][]) {
         this.mat = mat
         this.height = this.mat.length
         this.width = this.mat[0].length
     }
 
-    get = (h: number, w: number): [number, number, number] => {
+    get = (h: number, w: number): Pixel => {
         const hint = Math.round(h),
             wint = Math.round(w)
         if (0 <= hint && hint < this.height) {
@@ -20,6 +20,6 @@ export class PixelMatrix {
                 return this.mat[hint][wint]
             }
         }
-        return [0, 0, 0]
+        return [0, 0, 0, 0]
     }
 }
